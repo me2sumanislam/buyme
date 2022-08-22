bbdf1b7ee82163b06d7b17711696e6013c8373bf
me2sumonislam <me2sumanislam>
2022-08-23 00:11:16 +0600

    update all code new

--- football.js
+++ football.js
@@ -1,1 +1,10 @@
- 
+function addToCart(element){
+// console.log(element.parentNode.parentNode.children[0].innerText);
+
+const productName = element.parentNode.parentNode.children[0].innerText;
+console.log(productName);
+    
+const productPrice = element.parentNode.parentNode.children[1].innerText;
+console.log(productPrice);
+    
+}
