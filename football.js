4df2f5e8a3afe77229d8b84a81c9c3d96b13c886
me2sumonislam <me2sumanislam>
2022-08-23 23:21:37 +0600

    finally set part one

--- football.js
+++ football.js
@@ -1,10 +1,42 @@
-function addToCart(element){
-// console.log(element.parentNode.parentNode.children[0].innerText);
+ const cartArray =[];
 
-const productName = element.parentNode.parentNode.children[0].innerText;
-console.log(productName);
-    
-const productPrice = element.parentNode.parentNode.children[1].innerText;
-console.log(productPrice);
-    
+ function display(cartProduct){
+    // console.log(cartProduct);
+    const tableBody = document.getElementById("cart-products");
+    tableBody.innerHTML = " ";
+
+    for(let i = 0; i<cartProduct.length; i++){
+// console.log(cartArray[i].productName);
+const name = cartArray[i].productName;
+// console.log(name);
+
+const tr = document.createElement('tr');
+
+tr.innerHTML = `
+         <th>${i + 1}</th>
+         <th>${ name }</th>
+        
+        `;
+
+        tableBody.appendChild(tr);
+
+    }
+
 }
+
+
+ function addToCart(element){
+const productName=element.parentNode.parentNode.children[0].innerText;
+// console.log(productName);
+const productobj={
+    productName:productName,
+}
+// console.log(productobj);
+cartArray.push(productobj);
+// console.log(cartArray);
+document.getElementById("total-added-product").innerText=cartArray.length;
+
+document.getElementById("total-added-product").innerText = cartArray.length;
+display(cartArray);
+
+ }
